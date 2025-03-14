08056b0489a4daa9972091c77b27e42b5d6d29f2
saraa <saraa@Sara>
2025-03-14 19:57:20 +0100

    Merge con main

--- src/main/java/enviarSolicitud/Main.java
+++ src/main/java/enviarSolicitud/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		EnviarSolicitudModelo m=new EnviarSolicitudModelo();
 		EnviarSolicitudVista v=new EnviarSolicitudVista();
-		EnviarSolicitudControlador c=new EnviarSolicitudControlador(v,m);
+		EnviarSolicitudControlador c=new EnviarSolicitudControlador(v,m) ;
 		v.getFrame().setVisible(true);
 	}
 
