c73a5d2dd00c538af80d8b564fcde0a64ba9e004
saraa <saraa@Sara>
2025-03-16 22:11:49 +0100

    Pirmeros métodos modelo

--- src/main/java/cambiarEstadoSolicitud/CambiarEstadoSolicitudModelo.java
+++ src/main/java/cambiarEstadoSolicitud/CambiarEstadoSolicitudModelo.java
@@ -1,5 +1,22 @@
 package cambiarEstadoSolicitud;
 
-public class CambiarEstadoSolicitudModelo {
+import javax.swing.JOptionPane;
 
+import util.ApplicationException;
+import util.Database;
+
+public class CambiarEstadoSolicitudModelo {
+	private Database db=new Database();
+	public boolean EstaColegiado(String dni) {
+		String sql="SELECT COUNT(*) from Colegiados WHERE DNI = ?";	
+		Object[] numerocolegiados=db.executeQueryArray(sql,dni).get(0);
+		int numerocoleg=(int) numerocolegiados[0];
+	return numerocoleg>0;	
+	}
+	public boolean estaPendiente(String dni) {
+		String sql="SELECT estado_solicitud from Colegiados WHERE DNI = ?";
+		String estado=db.executeQueryArray(sql, dni).get(0)[0].toString();
+		return estado=="Pendiente";
+		
+	}
 }
