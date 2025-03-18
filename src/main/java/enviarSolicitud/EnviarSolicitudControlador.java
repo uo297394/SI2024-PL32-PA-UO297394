d7283985fe92faf85e3fc8c96ab362a350cbe20f
UO294851 <UO294851@GI-AN-B6-16.aulasuo.uniovi.es>
2025-03-18 17:42:22 +0100

    añadido metodo procesamiento de fichero

--- src/main/java/enviarSolicitud/EnviarSolicitudControlador.java
+++ src/main/java/enviarSolicitud/EnviarSolicitudControlador.java
@@ -2,10 +2,13 @@ package enviarSolicitud;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JTable;
@@ -64,5 +67,7 @@ public void generaFichero() {
         e.printStackTrace();
     }
 }
+//Funciones relativas a la historia modificar estado de la solicitud
 
+// MIRAR CLASE PRUEBA
 }
