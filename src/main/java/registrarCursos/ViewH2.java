300007871ad6b0ef3fbc052935fcc2274968d685
UO297394 <UO297394@GI-AN-B6-15.aulasuo.uniovi.es>
2025-03-18 16:11:57 +0100

    Merge de pagoInscripcion a main

--- src/main/java/registrarCursos/ViewH2.java
+++ src/main/java/registrarCursos/ViewH2.java
@@ -11,7 +11,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.text.ParseException;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Map; 
 
 public class ViewH2 extends JFrame {
     private JTextField txtTitulo;
